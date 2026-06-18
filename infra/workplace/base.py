3dfaac0332495108f9ff99ac959c0ad29715d119
Vova Polunin <blackgolub97@gmail.com>
2026-06-18 21:22:43 +0300

    fix: remove unnecessary interface (BaseWorkplaceRepository)

--- infra/workplace/base.py
+++ infra/workplace/base.py
@@ -4,15 +4,6 @@ from domain.entities.workspace import Workplace, Workspace
 from domain.values.booking import BookingTime
 from infra.base_repository import BaseRepository
 
-class BaseWorkplaceRepository(BaseRepository[Workplace]):
-    @abstractmethod
-    async def find_all_by_workspace(self, workspace_id: str) -> list[Workplace]:
-        ...
-    
-    @abstractmethod
-    async def find_all_available(self, workspace_id: str, bookind_time: BookingTime) -> list[Workplace]:
-        ...
-    
 class BaseWorkspaceRepository(BaseRepository[Workspace]):
     @abstractmethod
     async def find_all(self) -> list[Workspace]:
